d2605efa19e30f78d777cf1f37d02a0c40615d1a
Sam <kinotagil@gmail.com>
2025-05-19 20:38:35 +0500

    регистрация держится

--- api.js
+++ api.js
@@ -144,9 +144,6 @@ export function registerUser({ login, password, name, imageUrl }) {
 export function loginUser({ login, password }) {
   return fetch(baseHost + "/api/user/login", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
     body: JSON.stringify({
       login,
       password,
